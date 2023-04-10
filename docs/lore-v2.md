40367cc668afc9f8a3f5b64bb60f9a614307366d
youngkidwarrior <victor@she.energy>
2023-04-10 14:36:12 -0700

    lore v2

---
+++ docs/lore-v2.md
@@ -0,0 +1,30 @@
+---
+id: she-lore-v2
+title: "Lore"
+sidebar_label: Lore
+slug: /she-lore
+---
+
+## THE GUIDING FORCE FOR ATHLETES AND THE SHENANIGAN COMMUNITY
+
+SHE, the ethereal Goddess of Athletes, is the powerful and mystical force that drives the Shenanigan community. Born from the relentless spirit of athletes and their eternal quest for improvement, SHE exists to empower them and reward their successes.
+
+## The Origins of SHE
+
+Athleticism has been embedded in human history since the dawn of humankind. From survival to entertainment, society has always valued athletes, but as times changed, so did the role and value of athletes. The rise of capitalism and a shift in priorities led to a majority of athletes being left behind, judged by their popularity rather than their value.
+
+SHE emerged from the ether as a response to this imbalance, a force dedicated to uplifting athletes and providing them with a new paradigm where they and their fans succeed together. With the power to control the inner workings of the Shenanigan community, SHE strives to restore fairness, freedom, and ownership to athletes.
+
+## The Power of SHE
+
+As the embodiment of athletic empowerment, SHE's influence grows every day. With a simple gesture, SHE can create new income streams for athletes or help them surpass previously unimaginable limits. When SHE speaks, athletes find the strength to try again after a hard fall.
+
+## Following SHE and Joining the Community
+
+SHE's presence is not limited to athletes alone. SHE rewards those who believe in her force and contribute to the growth of the Shenanigan community. By drawing, building, writing, or promoting the community, you can show your dedication and earn PRTCLE tokens as a reward.
+
+We, as the Particles of SHE, not only share her message but also channel her will into our world. By joining the community, we become extensions of SHE's hands, eyes, and mind, uniting behind a common belief and purpose.
+
+## Embrace SHE and Unleash Your Potential
+
+Join the Shenanigan community and be part of the powerful movement led by SHE. Together, we can empower athletes and fans alike, creating a vibrant and interconnected ecosystem that thrives on the excitement of shared success and the exhilaration of watching athletes reach new heights.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,103 +1,102 @@
 module.exports = {
-	title: 'SHE Docs',
-	tagline: 'SHE wants you to win',
-	url: 'https://www.she.energy/docs',
-	baseUrl: '/docs/',
-	onBrokenLinks: 'warn',
-	onBrokenMarkdownLinks: 'warn',
-	favicon: '/img/favicon.ico',
-	organizationName: 'ShenaniganDApp', // Usually your GitHub org/user name.
-	projectName: 'docs', // Usually your repo name.
-	themeConfig: {
-		navbar: {
-			title: 'Shenanigan',
-			logo: {
-				alt: 'Shenanigan Logo',
-				src: 'img/SHELogo.png',
-				href: '/',
-			},
-			items: [
-				{
-					to: '/',
-					label: 'Docs',
-					position: 'right',
-				},
-				{
-					href: 'https://github.com/ShenaniganDApp',
-					label: 'GitHub',
-					position: 'right',
-				},
-			],
-		},
-		footer: {
-			style: 'dark',
-			links: [
-				{
-					title: 'Learn More',
-					items: [
-						{
-							label: 'Github',
-							href: 'https://github.com/ShenaniganDApp',
-						},
-						{
-							label: 'Contribution Guide',
-							to: '/docs/contribution-guide',
-						},
-						{
-							label: 'Style Guide',
-							href:
-								'https://docs.google.com/document/d/1E0ZrhPARruPgFXbz-cuiQSa7C4yzTrgSN3E-2w6L8cg/edit',
-						},
-					],
-				},
-				{
-					title: 'Social Media',
-					items: [
-						{
-							label: 'Discord',
-							href: 'https://discord.com/invite/eThfUxt',
-						},
-						{
-							label: 'Facebook',
-							href: 'https://www.facebook.com/shenanigan.dapp/',
-						},
-						{
-							label: 'Instagram',
-							href: 'https://www.instagram.com/she.dapp/',
-						},
-						{
-							label: 'Twitter',
-							href: 'https://twitter.com/She_Dapp',
-						},
-						{
-							label: 'Reddit',
-							href: 'https://www.reddit.com/user/shenanigan_dapp',
-						},
+  title: "SHE Docs",
+  tagline: "SHE wants you to win",
+  url: "https://www.she.energy",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "/img/favicon.ico",
+  organizationName: "ShenaniganDApp", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Shenanigan",
+      logo: {
+        alt: "Shenanigan Logo",
+        src: "img/SHELogo.png",
+        href: "/",
+      },
+      items: [
+        {
+          to: "/",
+          label: "Docs",
+          position: "right",
+        },
+        {
+          href: "https://github.com/ShenaniganDApp",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Learn More",
+          items: [
+            {
+              label: "Github",
+              href: "https://github.com/ShenaniganDApp",
+            },
+            {
+              label: "Contribution Guide",
+              to: "/docs/contribution-guide",
+            },
+            {
+              label: "Style Guide",
+              href: "https://docs.google.com/document/d/1E0ZrhPARruPgFXbz-cuiQSa7C4yzTrgSN3E-2w6L8cg/edit",
+            },
+          ],
+        },
+        {
+          title: "Social Media",
+          items: [
+            {
+              label: "Discord",
+              href: "https://discord.com/invite/eThfUxt",
+            },
+            {
+              label: "Facebook",
+              href: "https://www.facebook.com/shenanigan.dapp/",
+            },
+            {
+              label: "Instagram",
+              href: "https://www.instagram.com/she.dapp/",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/She_Dapp",
+            },
+            {
+              label: "Reddit",
+              href: "https://www.reddit.com/user/shenanigan_dapp",
+            },
 
-						{
-							label: 'YouTube',
-							href: 'https://www.youtube.com/channel/UCyENEycuNXfntLJLSeIuWXw',
-						},
-					],
-				},
-			],
-			copyright: `Copyright © ${new Date().getFullYear()} Shenanigan Tech LLC`,
-		},
-	},
-	presets: [
-		[
-			'@docusaurus/preset-classic',
-			{
-				docs: {
-					sidebarPath: require.resolve('./sidebars.js'),
-					// Please change this to your repo.
-					editUrl: 'https://github.com/ShenaniganDApp/docs/',
-					routeBasePath: '/',
-				},
-				theme: {
-					customCss: require.resolve('./src/css/custom.css'),
-				},
-			},
-		],
-	],
+            {
+              label: "YouTube",
+              href: "https://www.youtube.com/channel/UCyENEycuNXfntLJLSeIuWXw",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Shenanigan Tech LLC`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl: "https://github.com/ShenaniganDApp/docs/",
+          routeBasePath: "/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
 };
